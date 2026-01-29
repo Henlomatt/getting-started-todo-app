191beef38383f08e87a662ba4e0adfe2492b67e9
Henlomatt <henlomatt@gmail.com>
2026-01-29 11:17:45 +0100

    multiple greetings in getGreeting.js

--- backend/src/routes/getGreeting.js
+++ backend/src/routes/getGreeting.js
@@ -1,7 +1,13 @@
-const GREETING = 'Hello world!';
+const GREETING = [
+    'Hello world!',
+    'All hands on deck!',
+    'Carpe diem!',
+    'Seize the day!',
+    'To infinity and beyond!',
+];
 
 module.exports = async (req, res) => {
     res.send({
-        greeting: GREETING,
+        greeting: GREETING[Math.floor(Math.random() * GREETING.length)],
     });
 };
